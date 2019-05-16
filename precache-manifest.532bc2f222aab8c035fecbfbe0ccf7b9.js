2dfdc6fd9eb580ddd603df0ddc020eec9a79d71c
Komal Sarvani Mullapudi <ksmullap@uwaterloo.ca>
2019-05-16 13:04:08 -0400

    Updates

---
+++ precache-manifest.532bc2f222aab8c035fecbfbe0ccf7b9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cee8dc8b5ec33ace51e90974a39e39f",
+    "url": "/Casual-Crosswords/index.html"
+  },
+  {
+    "revision": "580d349a3d1f8a858c67",
+    "url": "/Casual-Crosswords/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "97480fd32d494869686b",
+    "url": "/Casual-Crosswords/static/css/main.6f450bb1.chunk.css"
+  },
+  {
+    "revision": "580d349a3d1f8a858c67",
+    "url": "/Casual-Crosswords/static/js/2.2918d66c.chunk.js"
+  },
+  {
+    "revision": "97480fd32d494869686b",
+    "url": "/Casual-Crosswords/static/js/main.0ca6ce85.chunk.js"
+  },
+  {
+    "revision": "548ab1c677b5958d0c3f",
+    "url": "/Casual-Crosswords/static/js/runtime~main.8c7e92df.js"
+  },
+  {
+    "revision": "acab4a81f9d7c82f826e1fc206e5e28f",
+    "url": "/Casual-Crosswords/static/media/crossword.acab4a81.jpg"
+  },
+  {
+    "revision": "48991312402db58d31856d2f347bdc89",
+    "url": "/Casual-Crosswords/static/media/dies.48991312.png"
+  },
+  {
+    "revision": "1d2a07603e7985dde0fbc2d445b42d0c",
+    "url": "/Casual-Crosswords/static/media/noctis.1d2a0760.png"
+  },
+  {
+    "revision": "dacf865fddd5056602e48bd51a701337",
+    "url": "/Casual-Crosswords/static/media/try_again.dacf865f.jpg"
+  },
+  {
+    "revision": "e0419573cbf3c8193e77cfee2af3160b",
+    "url": "/Casual-Crosswords/static/media/vesperum.e0419573.png"
+  },
+  {
+    "revision": "a5e8c353a00b09adc2c1fe4830adeb8b",
+    "url": "/Casual-Crosswords/static/media/woohoo.a5e8c353.jpg"
+  }
+]);
